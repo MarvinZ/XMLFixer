41ca566b8406f2bda6061a45b3b10a22140f8247
Marvin Zumbado <marvinzzz@gmail.com>
2021-09-24 13:18:50 -0600

    Add project files.

---
+++ XMLFixer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using U8Xml;
+
+namespace XMLFixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (Stream stream = File.OpenRead("C:/test/x.xml"))
+            using (XmlObject xml = XmlParser.Parse(stream))
+            {
+                XmlNode root = xml.Root;
+                string rootName = root.Name.ToString();         // "SomeData"
+                foreach (var child in root.Children)
+                {
+                    XmlNode kid = child;
+                    XmlAttribute attr = kid.Attributes.First();
+                    string attrName = attr.Value.ToString();
+                    Console.WriteLine("Do you want to keep: "+attrName+" ?");
+                }
+                // node
+                //XmlNode child = root.Children.First();
+                //string childName = child.Name.ToString();       // "Data"
+                //Console.WriteLine(childName);
+                //string innerText = child.InnerText.ToString();  // "bbb"
+
+                //// attribute
+                //XmlAttribute attr = child.Attributes.First();
+                //string attrName = attr.Name.ToString();         // "aa"
+                //int attrValue = attr.Value.ToInt32();           // 20
+
+
+            }
+            // *** DO NOT use any object from the parser. ***
+            // XmlObject, XmlNode, RawString, etc... are no longer accessible here.
+            // Their all memories are released when XmlObject.Dispose() called !!
+            // They must be evaluated and converted to string, int, and so on.
+            Console.ReadLine();
+        }
+    }
+}
